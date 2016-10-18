2210dcd482bbc96cc0bd712f04d78eb36d702d3c
skoryupina <e.skoryupina.develop@ya.ru>
2016-10-18 09:40:52 +0400

    added district attribute to the model for creation of new entity and persisting methods

--- src/main/java/com/skoryupina/controllers/DistrictController.java
+++ src/main/java/com/skoryupina/controllers/DistrictController.java
@@ -1,9 +1,11 @@
 package com.skoryupina.controllers;
 
+import com.skoryupina.entities.District;
 import com.skoryupina.services.DistrictService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -19,7 +21,15 @@ public class DistrictController {
 
     @RequestMapping(value = "/districts", method = RequestMethod.GET)
     public String list(Model model) {
+        District district = new District();
         model.addAttribute("districts", districtService.listAllDistricts());
+        model.addAttribute("district", district);
+        return "districts";
+    }
+
+    @PostMapping("/district")
+    public String districtSubmit(District district){
+        districtService.saveDistrict(district);
         return "districts";
     }
 }
